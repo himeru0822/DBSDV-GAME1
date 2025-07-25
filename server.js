720046d7de46d0a63b76cb3b4935abce6045343d
himeru0822 <la.graine.0822@gmail.com>
2025-07-25 18:04:18 +0900

    corsの導入

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const bcrypt = require('bcrypt');
 const User = require('./models/User');
 
 const app = express();
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
